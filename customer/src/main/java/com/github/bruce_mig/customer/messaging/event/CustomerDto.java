ac6ed7a376e053e9a6316d32746b470fde93db53
bruce-mig <brucetmig@icloud.com>
2025-07-05 16:28:46 +0200

    added ssn

--- customer/src/main/java/com/github/bruce_mig/customer/messaging/event/CustomerDto.java
+++ customer/src/main/java/com/github/bruce_mig/customer/messaging/event/CustomerDto.java
@@ -1,4 +1,4 @@
-package com.github.bruce_mig.customer.controller.dto;
+package com.github.bruce_mig.customer.messaging.event;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
--- customer/src/main/java/com/github/bruce_mig/customer/service/CustomerServiceImpl.java
+++ customer/src/main/java/com/github/bruce_mig/customer/service/CustomerServiceImpl.java
@@ -1,18 +1,23 @@
 package com.github.bruce_mig.customer.service;
 
+import com.github.bruce_mig.customer.controller.mapper.CustomerMapper;
 import com.github.bruce_mig.customer.domain.Customer;
 import com.github.bruce_mig.customer.domain.EmailAddress;
+import com.github.bruce_mig.customer.messaging.event.CustomerDto;
+import com.github.bruce_mig.customer.messaging.event.CustomerEvent;
 import com.github.bruce_mig.customer.repository.CustomerRepository;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Sinks;
+
+import java.time.Instant;
 
 @Service
 public class CustomerServiceImpl implements CustomerService{
 
     private final CustomerRepository customerRepository;
-    private final Sinks.Many<Customer> customerProducer;
+    private final Sinks.Many<CustomerEvent> customerProducer;
 
-    public CustomerServiceImpl(CustomerRepository customerRepository, Sinks.Many<Customer> customerProducer) {
+    public CustomerServiceImpl(CustomerRepository customerRepository, Sinks.Many<CustomerEvent> customerProducer) {
         this.customerRepository = customerRepository;
         this.customerProducer = customerProducer;
     }
@@ -20,7 +25,9 @@ public class CustomerServiceImpl implements CustomerService{
     @Override
     public Customer create(final Customer customer) {
         Customer customerCreated = customerRepository.save(customer);
-        customerProducer.tryEmitNext(customerCreated);
+
+        var customerCreatedEvent = new CustomerEvent.CustomerCreated(customerCreated.getId(), Instant.now(), CustomerMapper.mapToCustomerDto(customerCreated));
+        customerProducer.tryEmitNext(customerCreatedEvent);
         return customerCreated;
     }
 
@@ -31,5 +38,16 @@ public class CustomerServiceImpl implements CustomerService{
         customer.changeEmail(emailAddress);
         customerRepository.save(customer);
 
+    }
+
+    interface CustomerMapper{
+        static CustomerDto mapToCustomerDto(final Customer customerCreated){
+            return new CustomerDto(customerCreated.getFirstName().getValue(),
+                    customerCreated.getLastName().getValue(),
+                    customerCreated.getBirthDate().getValue(),
+                    customerCreated.getEmailAddress().getValue(),
+                    // todo: add ssn
+                    );
+        }
     }
 }
