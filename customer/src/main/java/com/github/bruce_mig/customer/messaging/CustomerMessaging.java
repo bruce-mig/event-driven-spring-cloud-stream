b9692a11c17ebc359407b8abae9487a40783788a
bruce-mig <brucetmig@icloud.com>
2025-07-05 16:29:08 +0200

    customer event

--- customer/src/main/java/com/github/bruce_mig/customer/messaging/CustomerMessaging.java
+++ customer/src/main/java/com/github/bruce_mig/customer/messaging/CustomerMessaging.java
@@ -1,6 +1,7 @@
 package com.github.bruce_mig.customer.messaging;
 
 import com.github.bruce_mig.customer.domain.*;
+import com.github.bruce_mig.customer.messaging.event.CustomerEvent;
 import com.github.bruce_mig.customer.service.CustomerService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,12 +20,12 @@ public class CustomerMessaging {
     }
 
     @Bean
-    public Sinks.Many<Customer> customerProducer(){
+    public Sinks.Many<CustomerEvent> customerProducer(){
         return Sinks.many().replay().latest();
     }
 
     @Bean
-    public Supplier<Flux<Customer>> customerSupplier(){
+    public Supplier<Flux<CustomerEvent>> customerSupplier(){
         return () ->  customerProducer().asFlux();
     }
 }
