1babcb440f9a4a951bacc6a8e07a92cabc279f55
bruce-mig <brucetmig@icloud.com>
2025-07-05 14:21:26 +0200

    stream bridge

--- customer/src/main/java/com/github/bruce_mig/customer/service/CustomerServiceImpl.java
+++ customer/src/main/java/com/github/bruce_mig/customer/service/CustomerServiceImpl.java
@@ -3,20 +3,25 @@ package com.github.bruce_mig.customer.service;
 import com.github.bruce_mig.customer.domain.Customer;
 import com.github.bruce_mig.customer.domain.EmailAddress;
 import com.github.bruce_mig.customer.repository.CustomerRepository;
+import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerServiceImpl implements CustomerService{
 
     private final CustomerRepository customerRepository;
+    private final StreamBridge streamBridge;
 
-    public CustomerServiceImpl(CustomerRepository customerRepository) {
+    public CustomerServiceImpl(CustomerRepository customerRepository, StreamBridge streamBridge) {
         this.customerRepository = customerRepository;
+        this.streamBridge = streamBridge;
     }
 
     @Override
     public Customer create(final Customer customer) {
-        return customerRepository.save(customer);
+        Customer customerCreated = customerRepository.save(customer);
+        streamBridge.send("customer", customerCreated);
+        return customerCreated;
     }
 
     @Override
