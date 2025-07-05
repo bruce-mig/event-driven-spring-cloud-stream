cb9d5bea8652ec25f78c2b983acb8f574395c781
bruce-mig <brucetmig@icloud.com>
2025-07-05 16:27:54 +0200

    created mapper interface

--- customer/src/main/java/com/github/bruce_mig/customer/controller/CustomerController.java
+++ customer/src/main/java/com/github/bruce_mig/customer/controller/CustomerController.java
@@ -1,6 +1,7 @@
 package com.github.bruce_mig.customer.controller;
 
 import com.github.bruce_mig.customer.controller.dto.CustomerDto;
+import com.github.bruce_mig.customer.controller.mapper.CustomerMapper;
 import com.github.bruce_mig.customer.domain.*;
 import com.github.bruce_mig.customer.service.CustomerService;
 import jakarta.validation.Valid;
@@ -20,14 +21,10 @@ public class CustomerController {
 
     @PostMapping
     public ResponseEntity<Customer> create(@Valid @RequestBody CustomerDto customerDto){
-        FirstName firstName = FirstName.of(customerDto.firstName());
-        LastName lastName = LastName.of(customerDto.lastName());
-        BirthDate birthDate = BirthDate.of(customerDto.birthDate());
-        EmailAddress emailAddress = EmailAddress.of(customerDto.emailAddress());
 
-        Customer customer = Customer.create(firstName, lastName, birthDate, emailAddress);
-
+        Customer customer = CustomerMapper.mapToCustomer(customerDto);
         Customer createdCustomer = customerService.create(customer);
+
         return ResponseEntity.ok(createdCustomer); // dont expose domain model outside app
     }
 
---
+++ customer/src/main/java/com/github/bruce_mig/customer/controller/mapper/CustomerMapper.java
@@ -0,0 +1,16 @@
+package com.github.bruce_mig.customer.controller.mapper;
+
+import com.github.bruce_mig.customer.controller.dto.CustomerDto;
+import com.github.bruce_mig.customer.domain.*;
+
+public interface CustomerMapper {
+
+    static Customer mapToCustomer(final CustomerDto customerDto){
+        FirstName firstName = FirstName.of(customerDto.firstName());
+        LastName lastName = LastName.of(customerDto.lastName());
+        BirthDate birthDate = BirthDate.of(customerDto.birthDate());
+        EmailAddress emailAddress = EmailAddress.of(customerDto.emailAddress());
+
+        return Customer.create(firstName, lastName, birthDate, emailAddress);
+    }
+}
