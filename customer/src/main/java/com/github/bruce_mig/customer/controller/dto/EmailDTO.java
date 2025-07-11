af731fcb15323e5cdfe24d7f3889f67946fbe475
bruce-mig <brucetmig@icloud.com>
2025-07-11 16:40:39 +0200

    added EmailDTO

---
+++ customer/src/main/java/com/github/bruce_mig/customer/controller/dto/EmailDTO.java
@@ -0,0 +1,9 @@
+package com.github.bruce_mig.customer.controller.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record EmailDTO(@NotBlank(message = "emailAddress cannot be blank")
+                        @Email(message = "please provide a valid email address")
+                        String emailAddress) {
+}
